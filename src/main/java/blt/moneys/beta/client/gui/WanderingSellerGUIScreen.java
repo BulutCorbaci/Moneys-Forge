6b4ff8deb148f80761285e21e669d963dd1434ab
BuluttishTheGuy <78735325+BulutCorbaci@users.noreply.github.com>
2021-11-17 15:57:45 +0300

    Update Output.jar

--- src/main/java/blt/moneys/beta/client/gui/WanderingSellerGUIScreen.java
+++ src/main/java/blt/moneys/beta/client/gui/WanderingSellerGUIScreen.java
@@ -5,8 +5,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.Minecraft;
 
 import java.util.HashMap;
@@ -51,6 +53,25 @@ public class WanderingSellerGUIScreen extends AbstractContainerScreen<WanderingS
 		int k = (this.width - this.imageWidth) / 2;
 		int l = (this.height - this.imageHeight) / 2;
 		this.blit(ms, k, l, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 186, 62, 0, 0, 8, 16, 8, 16);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 204, 62, 0, 0, 8, 16, 8, 16);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 222, 62, 0, 0, 8, 16, 8, 16);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 195, 62, 0, 0, 8, 16, 8, 16);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 213, 62, 0, 0, 8, 16, 8, 16);
+
+		RenderSystem.setShaderTexture(0, new ResourceLocation("moneys:textures/arrow_side.png"));
+		this.blit(ms, 231, 62, 0, 0, 8, 16, 8, 16);
+
 		RenderSystem.disableBlend();
 	}
 
@@ -70,6 +91,8 @@ public class WanderingSellerGUIScreen extends AbstractContainerScreen<WanderingS
 
 	@Override
 	protected void renderLabels(PoseStack poseStack, int mouseX, int mouseY) {
+		drawString(poseStack, this.font, "Input", 141 - this.leftPos, 62 - this.topPos, -12829636);
+		drawString(poseStack, this.font, "Output", 258 - this.leftPos, 62 - this.topPos, -12829636);
 	}
 
 	@Override
@@ -82,5 +105,9 @@ public class WanderingSellerGUIScreen extends AbstractContainerScreen<WanderingS
 	public void init() {
 		super.init();
 		this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
+		this.addRenderableWidget(new Button(150, 98, 56, 20, new TextComponent("< Back"), e -> {
+		}));
+		this.addRenderableWidget(new Button(222, 98, 56, 20, new TextComponent("Next >"), e -> {
+		}));
 	}
 }
